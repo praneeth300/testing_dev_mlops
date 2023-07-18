409708711cb9eef3909aeeddd80caaa1cdb643d6
Praneeth <64009514+praneeth300@users.noreply.github.com>
2023-07-18 19:01:39 +0530

    Update prep.py

--- jobs/autompg_pipeline/src/data_prep/prep.py
+++ jobs/autompg_pipeline/src/data_prep/prep.py
@@ -3,46 +3,41 @@ import argparse
 
 import logging
 import mlflow
+from pathlib import Path
 
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
 
-def main():
-    """Main function of the script."""
+# input and output arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--data", type=str, help="path to input data")
+parser.add_argument("--test_train_ratio", type=float, required=False, default=0.2)
+parser.add_argument("--train_data", type=str, help="path to train data")
+parser.add_argument("--test_data", type=str, help="path to test data")
+args = parser.parse_args()
 
-    # input and output arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, help="path to input data")
-    parser.add_argument("--test_train_ratio", type=float, required=False, default=0.2)
-    parser.add_argument("--train_data", type=str, help="path to train data")
-    parser.add_argument("--test_data", type=str, help="path to test data")
-    args = parser.parse_args()
+# Start Logging
+mlflow.start_run()
 
-    # Start Logging
-    mlflow.start_run()
+print(" ".join(f"{k}={v}" for k, v in vars(args).items()))
 
-    print(" ".join(f"{k}={v}" for k, v in vars(args).items()))
+print("input data:", args.data)
 
-    print("input data:", args.data)
+df = pd.read_csv(args.data)
 
-    df = pd.read_csv(args.data)
+train_df, test_df = train_test_split(
+    df,
+    test_size=args.test_train_ratio,
+    random_state=42
+)
 
-    train_df, test_df = train_test_split(
-        df,
-        test_size=args.test_train_ratio,
-        random_state=42
-    )
+# output paths are mounted as folder, therefore, we are adding a filename to the path
+train_df = train_df.to_csv((Path(args.train_data) / "train_data.csv"))
 
-    # output paths are mounted as folder, therefore, we are adding a filename to the path
-    train_df.to_csv(os.path.join(args.train_data, "data.csv"), index=False)
+test_df = test_df.to_csv((Path(args.test_data) / "test_data.csv"))
 
-    test_df.to_csv(os.path.join(args.test_data, "data.csv"), index=False)
+# Stop Logging
+mlflow.end_run()
 
-    # Stop Logging
-    mlflow.end_run()
-
-
-if __name__ == "__main__":
-    main()
