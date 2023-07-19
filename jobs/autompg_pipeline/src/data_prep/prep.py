d744b0987ffc165a01194cc703379065eea4308a
Praneeth <64009514+praneeth300@users.noreply.github.com>
2023-07-19 11:28:13 +0530

    Update prep.py

--- jobs/autompg_pipeline/src/data_prep/prep.py
+++ jobs/autompg_pipeline/src/data_prep/prep.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-
 # input and output arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--data", type=str, help="path to input data")
@@ -22,35 +21,27 @@ mlflow.start_run()
 
 print(" ".join(f"{k}={v}" for k, v in vars(args).items()))
 
-#print("input data:", args.data)
-
 arr = os.listdir(args.data)
 print(arr)
 
-########################################
+# Initialize an empty list to store DataFrames
 df = []
+
 for filename in arr:
     print("reading file: %s ..." % filename)
     with open(os.path.join(args.data, filename), "r") as handle:
-        # print (handle.read())
-        # ('input_df_%s' % filename) = pd.read_csv((Path(args.training_data) / filename))
         input_df = pd.read_csv((Path(args.data) / filename))
         df.append(input_df)
-        
-print(df)
 
-##########################################
+# Concatenate the list of DataFrames into a single DataFrame
+df = pd.concat(df)
 
-# df = pd.read_csv(args.data)
-# print(df)
+# Now you can split the DataFrame into train and test sets
+train_df, test_df = train_test_split(df, test_size=0.3, random_state=4)
 
-train_df, test_df = train_test_split(df,test_size=0.3,random_state=4)
-
-# output paths are mounted as folder, therefore, we are adding a filename to the path
-train_df = train_df.to_csv((Path(args.train_data) / "train_data.csv"))
-
-test_df = test_df.to_csv((Path(args.test_data) / "test_data.csv"))
+# Save the train and test DataFrames to CSV files
+train_df.to_csv((Path(args.train_data) / "train_data.csv"), index=False)
+test_df.to_csv((Path(args.test_data) / "test_data.csv"), index=False)
 
 # Stop Logging
 mlflow.end_run()
-
